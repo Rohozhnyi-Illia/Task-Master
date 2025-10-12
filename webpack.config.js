d633a75827ac0d0daaf01b8a8cd5bc9be246667c
Illia Rohozhnyi <rohozhnyiillia@gmail.com>
2025-10-13 00:08:48 +0200

    chore: setup project structure and add favicon

---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const isProduction = process.env.NODE_ENV === 'production'
+const publicPath = '/'
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[contenthash].js',
+    publicPath,
+    clean: true, // дополнительно очищает dist при сборке
+  },
+  mode: isProduction ? 'production' : 'development',
+  devtool: isProduction ? 'source-map' : 'eval-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@pages': path.resolve(__dirname, 'src/pages/'),
+      '@services': path.resolve(__dirname, 'src/services/'),
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@assets': path.resolve(__dirname, 'src/assets/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@store': path.resolve(__dirname, 'src/store/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: { postcssOptions: { plugins: [['autoprefixer']] } },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: { filename: 'images/[name].[contenthash][ext]' },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: { filename: 'fonts/[name].[contenthash][ext]' },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/styles.[contenthash].css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'public', to: '', globOptions: { ignore: ['**/index.html'] } }],
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    static: path.join(__dirname, 'dist'),
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        reactVendor: {
+          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+          name: 'react-vendors',
+          chunks: 'all',
+        },
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+          priority: -10,
+        },
+      },
+    },
+    runtimeChunk: 'single',
+  },
+}
