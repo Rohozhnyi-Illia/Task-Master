c18518c8ce8ad797615e6b5643996d98b1cd0a72
Illia Rohozhnyi <rohozhnyiillia@gmail.com>
2025-10-26 18:00:26 +0100

    fix: handle SPA routes on refresh via Netlify redirects

--- webpack.config.js
+++ webpack.config.js
@@ -4,88 +4,108 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-const plugins = [
-  new CleanWebpackPlugin(),
-  new HtmlWebpackPlugin({
-    template: './public/index.html',
-    inject: 'body',
-  }),
-  new MiniCssExtractPlugin({
-    filename: 'css/styles.[contenthash].css',
-  }),
-  new CopyWebpackPlugin({
-    patterns: [
-      {
-        from: 'public',
-        to: '',
-        globOptions: { ignore: ['**/index.html'] },
-      },
-    ],
-  }),
-]
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production'
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'js/[name].[contenthash].js',
-    publicPath: '/',
-    clean: true,
-  },
-  mode: 'production',
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components/'),
-      '@pages': path.resolve(__dirname, 'src/pages/'),
-      '@services': path.resolve(__dirname, 'src/services/'),
-      '@utils': path.resolve(__dirname, 'src/utils/'),
-      '@assets': path.resolve(__dirname, 'src/assets/'),
-      '@styles': path.resolve(__dirname, 'src/styles/'),
-      '@store': path.resolve(__dirname, 'src/store/'),
+  const plugins = [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      inject: 'body',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          to: '',
+          globOptions: { ignore: ['**/index.html'] },
+        },
+      ],
+    }),
+  ]
+
+  if (isProduction) {
+    plugins.unshift(
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: 'css/styles.[contenthash].css',
+      })
+    )
+  }
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: isProduction ? 'js/[name].[contenthash].js' : 'js/[name].js',
+      publicPath: '/',
+      clean: true,
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
-        generator: { filename: 'images/[name].[contenthash][ext]' },
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        generator: { filename: 'fonts/[name].[contenthash][ext]' },
-      },
-    ],
-  },
-  plugins,
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        reactVendor: {
-          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-          name: 'react-vendors',
-          chunks: 'all',
-        },
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-          priority: -10,
-        },
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components/'),
+        '@pages': path.resolve(__dirname, 'src/pages/'),
+        '@services': path.resolve(__dirname, 'src/services/'),
+        '@utils': path.resolve(__dirname, 'src/utils/'),
+        '@assets': path.resolve(__dirname, 'src/assets/'),
+        '@styles': path.resolve(__dirname, 'src/styles/'),
+        '@store': path.resolve(__dirname, 'src/store/'),
       },
     },
-    runtimeChunk: 'single',
-  },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset/resource',
+          generator: { filename: 'images/[name].[contenthash][ext]' },
+        },
+        {
+          test: /\.(woff2?|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: { filename: 'fonts/[name].[contenthash][ext]' },
+        },
+      ],
+    },
+    plugins,
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          reactVendor: {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name: 'react-vendors',
+            chunks: 'all',
+          },
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+            priority: -10,
+          },
+        },
+      },
+      runtimeChunk: 'single',
+    },
+    devServer: {
+      static: path.resolve(__dirname, 'public'),
+      port: 3000,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+    },
+  }
 }
