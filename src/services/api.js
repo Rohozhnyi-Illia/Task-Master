75fed25d283d8a59f0dbb1802a873611605409e6
Illia Rohozhnyi <rohozhnyiillia@gmail.com>
2025-10-31 23:42:34 +0100

    chore: add axios request timeout and handle connection delays

--- src/services/api.js
+++ src/services/api.js
@@ -6,6 +6,7 @@ const api = axios.create({
   // baseURL: 'http://localhost:9000/api',
   baseURL: 'https://taskmaster-backend-e940.onrender.com/api',
   withCredentials: true,
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
